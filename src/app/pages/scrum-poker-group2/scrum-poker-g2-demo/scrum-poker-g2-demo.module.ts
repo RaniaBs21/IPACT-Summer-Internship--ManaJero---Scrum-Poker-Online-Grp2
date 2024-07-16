2bc663b2ff1766ea3ddcae96668fdb60a25afdcf
RaniaBs21 <raniabensalem53@gmail.com>
2024-07-16 16:39:33 +0100

    demo dynamique

--- src/app/pages/scrum-poker-group2/scrum-poker-g2-demo/scrum-poker-g2-demo.module.ts
+++ src/app/pages/scrum-poker-group2/scrum-poker-g2-demo/scrum-poker-g2-demo.module.ts
@@ -19,20 +19,22 @@ import {ScrumPokerG2DemoComponent} from './scrum-poker-g2-demo.component';
 // import {StepperComponent} from './stepper/stepper.component';
 import {RouterLink} from '@angular/router';
 import { DemoUpdateComponent } from './demo-update/demo-update.component';
-// import { BenefitsUpdateComponent } from './benefits-update/benefits-update.component';
-import { LimitsUpdateComponent } from './limits-update/limits-update.component';
-import { BenefitsAddComponent } from './benefits-add/benefits-add.component';
-// import { LimitssAddComponent } from './limitss-add/limitss-add.component';
 import {DemoFormComponent} from './demo-form/demo-form.component';
-import { NewsUpdateComponent } from './news-update/news-update.component';
+import {LimitsUpdateComponent} from './limits-update/limits-update.component';
+import {BenefitsAddComponent} from './benefits-add/benefits-add.component';
+import {BenifitsUpdateComponent} from './benifits-update/benifits-update.component';
 import {StepsUpdateComponent} from './steps/steps-update/steps-update.component';
 import {StepCreateComponent} from './steps/step-create/step-create.component';
-import {BenifitsUpdateComponent} from './benifits-update/benifits-update.component';
-import { LimitssAddComponent } from './limitss-add/limitss-add.component';
-import { SecurityCamerasComponent } from './security-cameras/security-cameras.component';
-import { IntroUpdateComponent } from './intro-update/intro-update.component';
-import { DiagramAddComponent } from './diagram-add/diagram-add.component';
-import { DiagramUpdateComponent } from './diagram-update/diagram-update.component';
+import {NewsUpdateComponent} from './news-update/news-update.component';
+import {LimitssAddComponent} from './limitss-add/limitss-add.component';
+import {SecurityCamerasComponent} from './security-cameras/security-cameras.component';
+import {IntroUpdateComponent} from './intro-update/intro-update.component';
+import {DiagramAddComponent} from './diagram-add/diagram-add.component';
+import {DiagramUpdateComponent} from './diagram-update/diagram-update.component';
+// import { BenefitsUpdateComponent } from './benefits-update/benefits-update.component';
+
+// import { LimitssAddComponent } from './limitss-add/limitss-add.component';
+
 
 @NgModule({
   imports: [
